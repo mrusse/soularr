14588a66a3a6820d4718581d34839e05e191d21b
Michael <99michaelrussell99@gmail.com>
2024-09-22 11:48:32 -0400

    Album downloading should work but all downloads getting cancelled.

--- soularr.py
+++ soularr.py
@@ -1,5 +1,38 @@
+import difflib
+import os
 from pyarr import LidarrAPI
 import slskd_api
+
+def album_match(lidarr_tracks, slskd_tracks):
+    counted = []
+
+    for lidarr_track in lidarr_tracks:
+        lidarr_filename = lidarr_track['title'] + ".flac"
+
+        for slskd_track in slskd_tracks:
+            slskd_filename = slskd_track['filename']
+            ratio = difflib.SequenceMatcher(None, lidarr_filename, slskd_filename).ratio()
+
+            if(difflib.SequenceMatcher(None, lidarr_filename, slskd_filename).ratio() > 0.4 and not lidarr_filename in counted):
+                counted.append(lidarr_filename)
+                print("Lidarr Filename: " + lidarr_filename + "\nSoulseek Filename: " + slskd_filename + "\nDiff Ratio: " + str(ratio) + "\n-------------------")
+                break
+
+    print("\nNum Matched: " + str(len(counted)) + " vs Total Num: " + str(len(lidarr_tracks)))
+
+    if(len(counted) == len(lidarr_tracks)):
+        print("SUCCESSFUL MATCH \n-------------------\n")
+        return True
+    print("FAILED MATCH \n-------------------\n")
+    return False
+
+def album_track_num(directory):
+    files = directory['files']
+    count = 0
+    for file in files:
+        if(".flac" in file['filename']):
+            count += 1
+    return count
 
 with open('slskd.auth', 'r') as file:
     slskd_api_key = file.read().replace('\n', '')
@@ -8,27 +41,39 @@ with open('lidarr.auth', 'r') as file:
     lidarr_api_key = file.read().replace('\n', '')
 
 host_url = 'http://192.168.2.190:8686'
-
+slskd = slskd_api.SlskdClient('http://192.168.2.190:5030', slskd_api_key, '/')
 lidarr = LidarrAPI(host_url, lidarr_api_key)
+
 artistName = lidarr.get_wanted()['records'][0]['artist']['artistName']
 artistID = lidarr.get_wanted()['records'][0]['artistId']
 albumID = lidarr.get_wanted()['records'][0]['id']
 
 tracks = lidarr.get_tracks(artistId = artistID, albumId = albumID)
-test_querry = artistName + " " + tracks[0]['trackNumber'] + " " + tracks[0]['title'] + ".flac"
+track_num = len(tracks)
 
-print(test_querry)
+for track in tracks:
+    querry = artistName + " " + track['trackNumber'] + " " + track['title'] + ".flac"
+    search = slskd.searches.search_text(searchText = querry, searchTimeout = 5000, filterResponses=True)
 
-slskd = slskd_api.SlskdClient('http://192.168.2.190:5030', slskd_api_key, '/')
-search = slskd.searches.search_text(test_querry)
+    while(True):
+        if slskd.searches.state(search['id'])['state'] != 'InProgress':
+            break
 
-while(True):
-    if slskd.searches.state(search['id'])['state'] != 'InProgress':
+    for result in slskd.searches.search_responses(search['id']):
+        username = result['username']
+        files = result['files']
+
+        for file in files:
+            if('.flac' in file['filename']):
+                file_dir = os.path.dirname(file['filename'])
+                directory = slskd.users.directory(username = username, directory = file_dir)
+                if(album_track_num(directory) == track_num):
+                    if(album_match(tracks, directory['files'])):
+                        slskd.transfers.enqueue(username=username, files=directory['files'])
+                        break
+        else:
+            continue
         break
-
-for result in slskd.searches.search_responses(search['id']):
-   if(len(result['files']) == 1 and '.flac' in result['files'][0]['filename']):
-        print(result['files'])
-        break
-
-slskd.transfers.enqueue(username=result['username'], files=result['files'])
+    else:
+        continue
+    break
