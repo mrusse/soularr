612848fd289444f559e676c35521a4caa0397957
AdamWHY2K <68286215+AdamWHY2K@users.noreply.github.com>
2024-10-17 01:01:39 +0100

    refactor: make subclass of Arrs

--- readarr.py
+++ readarr.py
@@ -1,10 +1,11 @@
-from pyarr import ReadarrAPI
+from arrs import Arrs
 from pyarr.types import JsonArray, JsonObject
 
-class Readarr:
-    def __init__(self: object, host_url: str, api_key: str, download_dir: str, accepted_formats: list[str]):
-        self.readarr = ReadarrAPI(host_url, api_key)
-        self.download_dir = download_dir
-        self.accepted_formats = accepted_formats
+class Readarr(Arrs):
+    def __init__(self: object, host_url: str, api_key: str, download_dir: str, accepted_formats: list[str], accepted_countries: list[str], number_of_books_to_grab: int, page_size: int) -> None:
+        super().__init__('readarr', host_url, api_key, download_dir, accepted_formats, accepted_countries, number_of_books_to_grab, page_size)
+
+    def get_wanted(self, page: int = 1) -> JsonObject:
+        return self.readarr.get_missing(page=page, page_size=self.page_size, sort_dir='ascending',sort_key='title')
 
     # TODO
