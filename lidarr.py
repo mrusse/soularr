a087a6b3f24ac298304acc77750699a9b96882ad
AdamWHY2K <68286215+AdamWHY2K@users.noreply.github.com>
2024-10-17 21:59:34 +0100

    refactor: WIP - grab_releases, probably needs to be more generic and moved to Arrs

--- lidarr.py
+++ lidarr.py
@@ -1,13 +1,28 @@
+from datetime import datetime
 from arrs import Arrs
 from pyarr.types import JsonArray, JsonObject
 
 class Lidarr(Arrs):
-    def __init__(self: object, host_url: str, api_key: str, download_dir: str, accepted_countries: list[str], accepted_formats: list[str], use_most_common_tracknum: bool, allow_multi_disc: bool, number_of_albums_to_grab: int, page_size: int) -> None:
-        super().__init__('lidarr', host_url, api_key, download_dir, accepted_formats, accepted_countries, number_of_albums_to_grab, page_size)
+    def __init__(
+            self,
+            host_url: str,
+            api_key: str,
+            download_dir: str,
+            accepted_countries: list[str],
+            accepted_formats: list[str],
+            use_most_common_tracknum: bool,
+            allow_multi_disc: bool,
+            number_of_albums_to_grab: int,
+            album_prepend_artist: bool,
+            search_for_tracks: bool,
+            remove_wanted_on_failure: bool
+        ) -> None:
+        super().__init__('lidarr', host_url, api_key, download_dir, accepted_formats, accepted_countries, number_of_albums_to_grab, album_prepend_artist, remove_wanted_on_failure)
         self.use_most_common_tracknum = use_most_common_tracknum
         self.allow_multi_disc = allow_multi_disc
+        self.search_for_tracks = search_for_tracks
 
-    def update_wanted(self, page: int = 1) -> JsonObject:
+    def get_wanted(self, page: int = 1) -> JsonObject:
         return self.lidarr.get_wanted(page=page, page_size=self.page_size, sort_dir='ascending',sort_key='albums.title')
 
     def release_track_count_mode(releases: JsonArray) -> int | None:
@@ -53,3 +68,57 @@ class Lidarr(Arrs):
         else:
             default_release = releases[0]
         return default_release
+    
+    def grab_album(self, record: JsonObject) -> tuple[str, JsonArray, str, JsonObject] | None:
+        artist_name = record['artist']['artistName']
+        artist_id = record['artistId']
+        album_id = record['id']
+        release = self.choose_release(album_id, artist_name)
+        release_id = release['id']
+        all_tracks = self.lidarr.get_tracks(artistId = artist_id, albumId = album_id, albumReleaseId = release_id)
+
+        # TODO: Right now if search_for_tracks is False. Multi disc albums will never be downloaded so we need to loop through media in releases even for albums
+        if len(release['media']) == 1:
+            album_title = self.lidarr.get_album(album_id)['title']
+            if self.is_blacklisted(album_title):
+                return (None, [], artist_name, release)
+            query = f"{artist_name} {album_title}" if self.prepend_creator or len(album_title) == 1 else album_title
+        return (query, all_tracks, artist_name, release)
+
+    def grab_releases(self, slskd_instance: object, wanted_records: JsonArray, failure_file_path: str) -> int:
+        failed_downloads = 0
+        for record in wanted_records:
+            (query, all_tracks, artist_name, release) = self.grab_album(record)
+            if query is None:
+                continue
+            print(f"Searching album: {query}")
+            success = slskd_instance.search_and_download(query, all_tracks, all_tracks[0], artist_name, release)
+
+            if not success and self.search_for_tracks:
+                for media in release['media']:
+                    tracks = []
+                    for track in all_tracks:
+                        if track['mediumNumber'] == media['mediumNumber']:
+                            tracks.append(track)
+                    for track in tracks:
+                        if self.is_blacklisted(track['title']):
+                            continue
+                        query = f"{artist_name} {track['title']}" if self.prepend_creator or len(track['title']) == 1 else track['title']
+                        print(f"Searching track: {query}")
+                        success = slskd_instance.search_and_download(query, tracks, track, artist_name, release)
+                        if success:
+                            break
+
+                    if not success:
+                        if self.remove_wanted_on_failure:
+                            print(f"ERROR: Failed to grab album: {record['title']} for artist: {artist_name}\n Failed album removed from wanted list and added to \"failure_list.txt\"")
+                            record['monitored'] = False
+                            self.lidarr.upd_album(record)
+                            with open(failure_file_path, "a") as file:
+                                file.write(f"{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} - {artist_name}, {record['title']}, {record["id"]}\n")
+                        else:
+                            print(f"ERROR: Failed to grab album: {record['title']} for artist: {artist_name}")
+                        failed_downloads += 1
+            # success = False
+
+        return failed_downloads
