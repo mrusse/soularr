fae38637586a692cefa1f945c22edf33a47ca9a7
AdamWHY2K <68286215+AdamWHY2K@users.noreply.github.com>
2024-10-19 16:19:57 +0100

    refactor: WIP - abstract methods implementation

--- readarr.py
+++ readarr.py
@@ -1,5 +1,5 @@
 from arrs import Arrs
-from pyarr.types import JsonArray, JsonObject
+from pyarr.types import JsonObject
 
 class Readarr(Arrs):
     def __init__(self: object, host_url: str, api_key: str, download_dir: str, accepted_formats: list[str], accepted_countries: list[str], number_of_books_to_grab: int, page_size: int) -> None:
@@ -7,5 +7,18 @@ class Readarr(Arrs):
 
     def get_wanted(self, page: int = 1) -> JsonObject:
         return self.readarr.get_missing(page=page, page_size=self.page_size, sort_dir='ascending',sort_key='title')
+    
+    def get_title(self, release: JsonObject) -> str:
+        # gotta check if bookId is the right key
+        return self.readarr.get_book(albumIds = release['bookId'])['title']
+    
+    def retag_file(self, release_name: str, filename: str, path: str, folder: dict) -> None:
+        # TODO: Likely needed for audiobooks, but I don't use readarr for audiobooks so idk
+        pass
+
+    def import_downloads(self, creator_folders: list[str]) -> None:
+        # gotta check if DownloadedBooksScan is the right command
+        # TODO
+        pass
 
     # TODO
