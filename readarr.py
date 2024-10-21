cc048cd5f99353438ecdb19f30fd0c317c5164fa
AdamWHY2K <68286215+AdamWHY2K@users.noreply.github.com>
2024-10-21 12:26:05 +0100

    refactor: remove unneeded params, add grab_book

--- readarr.py
+++ readarr.py
@@ -7,14 +7,14 @@ class Readarr(Arrs):
             host_url: str,
             api_key: str,
             download_dir: str,
-            current_page_file_path,
-            accepted_formats: list[str],
-            accepted_countries: list[str],
+            current_page_file_path: str,
             title_blacklist: list[str],
             number_of_books_to_grab: int,
-            page_size: int
+            search_type: str,
+            prepend_creator: bool,
+            remove_wanted_on_failure: bool
         ) -> None:
-        super().__init__('readarr', host_url, api_key, download_dir, current_page_file_path, accepted_formats, accepted_countries, title_blacklist, number_of_books_to_grab, page_size)
+        super().__init__('readarr', host_url, api_key, download_dir, current_page_file_path, title_blacklist, number_of_books_to_grab, search_type, prepend_creator, remove_wanted_on_failure)
 
     def get_wanted(self, page: int = 1) -> JsonObject:
         return self.readarr.get_missing(page=page, page_size=self.page_size, sort_dir='ascending',sort_key='title')
@@ -31,5 +31,14 @@ class Readarr(Arrs):
         # gotta check if DownloadedBooksScan is the right command
         # TODO
         pass
+
+    def grab_book(self, record: JsonObject) -> tuple[str, str]:
+        book = self.readarr.get_book(record['id'])
+        book_title = book['title']
+        author_name = book['author']['authorName']
+        if self.is_blacklisted(book_title):
+            return (None, author_name)
+        query = f"{author_name} {book_title}" if self.prepend_creator or len(book_title) == 1 else book_title
+        return (query, book_title)
 
     # TODO
