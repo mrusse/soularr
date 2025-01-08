2d7d1891ed2be3b1405a8ad37233f82ffb57e9a4
Michael <99michaelrussell99@gmail.com>
2025-01-08 12:06:09 -0500

    Fixed line endings in the run script

--- run.sh
+++ run.sh
@@ -2,14 +2,14 @@
 
 #This script is used when running the app through docker. It handles scheduling the script.
 
-# Default interval is 300 seconds (5 minutes) if not set
+#Default interval is 300 seconds (5 minutes) if not set
 INTERVAL=${SCRIPT_INTERVAL:-300}
 
 while true; do
     if ps aux | grep "[s]oularr.py" > /dev/null; then
         echo "Soularr is already running. Exiting..."
     else
-        # Pass in the arguments given to the bash script over to the Python script
+        #Pass in the arguments given to the bash script over to the Python script
         python -u /app/soularr.py "$@"
     fi
 
