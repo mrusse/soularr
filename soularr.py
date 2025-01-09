62c561d9b99c8454e628ec542bb56a48c4a09066
bjh1977 <bjh1977@users.noreply.github.com>
2025-01-09 12:30:49 +0000

    Update soularr.py

--- soularr.py
+++ soularr.py
@@ -660,8 +660,9 @@ try:
     total_wanted = wanted['totalRecords']
 
     if search_type == 'all':
+        wanted = lidarr.get_wanted(page=1, page_size=99999999, sort_dir='ascending',sort_key='albums.title', missing=missing)
+
         logger.info(f"Searching for a random selection of {number_of_albums_to_grab} wanted albums out of {total_wanted} total wanted albums...")
-        # get a random selection of wanted albums. either number_of_albums_to_grab or all of them if there are less than that
         wanted_records = random.sample(wanted['records'], min(number_of_albums_to_grab, total_wanted))       
 
     elif search_type == 'incrementing_page':
